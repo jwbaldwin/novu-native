1285b14294d7858784751e9ebde6c815ecfddac5
James Baldwin <jbaldwin@Quantum-book.fios-router.home>
2018-10-20 10:59:45 -0400

    initial native app

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">My Vue Native App</text>
+    <text class="text-color-secondary">By James Baldwin</text>
+    </view>
+</template>
+ 
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: black;
+}
+.text-color-secondary {
+  color: grey;
+}
+</style>
